23163e583565caefd58f39552c5b0c8c82dd9e39
YasmeenGad <gadyasmeen335@gmail.com>
2025-07-08 19:07:49 +0300

    feat: create auth mapper

---
+++ lib/features/auth/data/mapper/auth_mapper.dart
@@ -0,0 +1,13 @@
+import 'package:mastery_hub_its_task/features/auth/data/models/user_model.dart';
+
+import '../../domain/entities/user_entity.dart';
+
+class AuthMapper {
+  static UserEntity toEntity(UserModel userModel) {
+    return UserEntity(
+      uid: userModel.uid,
+      email: userModel.email,
+      userName: userModel.userName,
+    );
+  }
+}
