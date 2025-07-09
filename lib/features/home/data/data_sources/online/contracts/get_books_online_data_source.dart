bd30d2edbe8ce6f0c3075847b4b751c59bb80a82
YasmeenGad <gadyasmeen335@gmail.com>
2025-07-09 11:43:20 +0300

    feat: create get books data source

--- lib/features/home/data/data_sources/online/contracts/get_books_online_data_source.dart
+++ lib/features/home/data/data_sources/online/contracts/get_books_online_data_source.dart
@@ -1,4 +1,6 @@
-abstract class GetBooksOnlineDataSource {
-  Future<DataResult>
+import '../../../../../../core/networking/common/api_result.dart';
+import '../../../models/response/get_books_response_dto.dart';
 
+abstract class GetBooksOnlineDataSource {
+  Future<DataResult<GetBooksResponseDto>> getBooks();
 }
---
+++ lib/features/home/domain/entities/response/get_books_response_entity.dart
@@ -0,0 +1,14 @@
+class GetBooksResponseEntity {
+  final String? id;
+  final String? title;
+  final String? author;
+  final String? genre;
+  final int? yearPublished;
+
+  GetBooksResponseEntity({this.id,
+    this.title,
+    this.author,
+    this.genre,
+    this.yearPublished,});
+
+}
