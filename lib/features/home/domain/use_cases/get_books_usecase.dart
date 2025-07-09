81af3c2deff0c1c19bba4e310b84eff59ed08cfa
YasmeenGad <gadyasmeen335@gmail.com>
2025-07-09 22:59:06 +0300

    feat: create get books use case

---
+++ lib/features/home/domain/use_cases/get_books_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:mastery_hub_its_task/features/home/domain/contracts/home_repo.dart';
+
+import '../../../../core/networking/common/api_result.dart';
+import '../entities/response/get_books_response_entity.dart';
+
+class GetBooksUseCase {
+  final HomeRepo _repo;
+
+  const GetBooksUseCase(this._repo);
+
+  Future<DataResult<GetBooksResponseEntity>> call() => _repo.getBooks();
+}
